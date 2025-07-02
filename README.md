20202eb78bca4fba94e2eb7543d7b755df56c654
My Desktop <39316467+CraigRoss95@users.noreply.github.com>
2025-07-02 13:49:01 -0400

    Have AtomicCards.json load in via json library rather than using Pandas methond (much faster load times

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ https://mtgjson.com/downloads/all-files/#atomiccards
 - [X] Speed up loop by creating one list object (rather than make a series for every card and mash them together) 
 - [X] Create "working" branch
 - [X] Move var_types to JSON (and then get them from that file)
+- [X] Use json library to read in AtomicCards.json
 - [ ] Fixed DTypes (needed for SQL) 
 - [ ] File not found error message
 - [ ] Failed on CARDNAME error message
--- mtgPandas.py
+++ mtgPandas.py
@@ -3,18 +3,20 @@ import json
 
 pd.set_option('display.max_columns', None)
 
-
+print ("Loading var_types.json to object...")
 # TODO this is currently unused, use it when setting up col data types
 with open("var_types.json", "r") as file:
     var_types = json.load(file)
 
-#Get Raw Data
-df_raw = pd.read_json("AtomicCards.json")
-df_raw.drop(["date","version"], inplace=True)
-
+print ("Loading AtomicCards.json to object (this might take a while)...")
+with open("AtomicCards.json", "r", encoding="utf8") as file:
+    raw_json = json.load(file)
+    
+print ("converting JSON object to Data Frame readable content...")
 all_cards_list = []
-for index in (range(len(df_raw))):
-    for alternate in df_raw.iloc[index]["data"]:
+card_count = 0
+for json_key, json_value in raw_json["data"].items():
+    for alternate in json_value:
         card = alternate
         new_card = {}
         for key, value in var_types.items():
@@ -22,8 +24,15 @@ for index in (range(len(df_raw))):
                 new_card[key] = card[key]
             else:
                 new_card[key] = None
+        
         all_cards_list.append(new_card)
-    
+        card_count = card_count + 1
+    # if card_count % 1000 == 0:
+    #     print(card_count)
+print ("Converting to Pandas Data Frame")
 df = pd.DataFrame(all_cards_list)
 
 # print(df.dtypes)
+
+# this should be 32810
+print (f"{len(all_cards_list)} Cards loaded")
